2ba2e753892e4cd551e09b54f094a0647d6eb78d
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-08-14 17:46:50 -0500

    Initial commit

---
+++ ebay-organizer/src/main/java/launchcode/org/ebay/organizer/EbayOrganizerApplication.java
@@ -0,0 +1,13 @@
+package launchcode.org.ebay.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayOrganizerApplication.class, args);
+	}
+
+}
---
+++ ebay-organizer/src/test/java/launchcode/org/ebay/organizer/EbayOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package launchcode.org.ebay.organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbayOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
