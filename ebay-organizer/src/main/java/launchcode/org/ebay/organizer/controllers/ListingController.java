e138579bf962408e6bfaf3f17f10985aa07cee26
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-08-24 17:15:05 -0500

    Created home page

--- ebay-organizer/src/main/java/launchcode/org/ebay/organizer/controllers/ListingController.java
+++ ebay-organizer/src/main/java/launchcode/org/ebay/organizer/controllers/ListingController.java
@@ -1,7 +1,15 @@
 package launchcode.org.ebay.organizer.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("listings")
 public class ListingController {
+
+    @GetMapping
+    public String displayAllListings() {
+        return "index";
+    }
 }
--- ebay-organizer/src/main/java/launchcode/org/ebay/organizer/controllers/UserController.java
+++ ebay-organizer/src/main/java/launchcode/org/ebay/organizer/controllers/UserController.java
@@ -2,8 +2,10 @@ package launchcode.org.ebay.organizer.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
 
     @GetMapping
